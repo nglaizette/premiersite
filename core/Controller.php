07c5eea5d79f8c2ecfd8a1751e30395f88c9621e
NicoG <winnipeg.francois@gmail.com>
2023-04-22 00:15:23 +0200

    - Ajout default page - Prise en compte du rendu automatique

--- core/Controller.php
+++ core/Controller.php
@@ -3,7 +3,9 @@
 class Controller
 {
     public $request;
-    public $vars = [];
+    public $layout = 'default';
+    private $vars = [];
+    private $rendered = false;
 
     public function __construct($request)
     {
@@ -12,10 +14,18 @@ class Controller
 
     public function render($view)
     {
+        if ($this->rendered) {
+            return false;
+        }
         extract($this->vars);
         $view = ROOT.DS.'views'.DS.$this->request->controller.DS.$view.'.php';
+        ob_start();
 
         require $view;
+        $content_for_layout = ob_get_clean();
+
+        require ROOT.DS.'views'.DS.'layout'.DS.$this->layout.'.php';
+        $this->rendered = true;
     }
 
     public function set($key, $value)
